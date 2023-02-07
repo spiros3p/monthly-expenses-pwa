debdfa636afbc0150c1dab42e21a6a1391d4bd7b
Spyridon Peppas <spiros3p@gmail.com>
2023-02-07 01:29:17 +0100

    added jest, spectator, playwrite

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js (javascript file)
+module.exports = {
+    preset: "jest-preset-angular",
+    setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+    testMatch: [
+        "<rootDir>/src/**/*.spec.ts"
+    ]
+};
