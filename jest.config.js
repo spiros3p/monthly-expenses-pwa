93987a61a575f5df2ff7a52781198f927d9328d7
Spyridon Peppas <spyridonpeppas@Spyridons-MacBook-Pro.local>
2023-05-07 15:44:27 +0200

    clean up master

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js (javascript file)
-module.exports = {
-    preset: "jest-preset-angular",
-    setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-    testMatch: [
-        "<rootDir>/src/**/*.spec.ts"
-    ],
-    collectCoverage: true
-};
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  timeOfTheDay: '9',
-  currency: '€',
-  theme: 'light'
-};
