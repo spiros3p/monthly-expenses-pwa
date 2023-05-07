f2bca6bcae1c3e7f20020f98f2cfb1e677532f4f
Spyridon Peppas <spyridonpeppas@Spyridons-MacBook-Pro.local>
2023-05-07 20:44:41 +0200

    test

--- build_deploy_push.py
+++ build_deploy_push.py
@@ -2,6 +2,12 @@ import os
 
 x = input('Commit message: ')
 
+
+try:
+    os.system('rm -R docs')
+except:
+    print("'docs' directory does not exist")
+
 os.system('npm run build:ionic')
 os.system('cp -R build docs')
 os.system('git add .')
